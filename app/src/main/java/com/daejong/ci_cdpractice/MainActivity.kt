cd30414814578a5098662f480ec9d0fefbf62cfa
daejong <teddy8653@gmail.com>
2026-02-28 16:00:40 +0900

    ci/cd test_mac 1

--- app/src/main/java/com/daejong/ci_cdpractice/MainActivity.kt
+++ app/src/main/java/com/daejong/ci_cdpractice/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         mBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        mBinding.btnA.setOnClickListener { Toast.makeText(this, "테스트 토스트", Toast.LENGTH_SHORT).show() }
+        mBinding.btnA.setOnClickListener { Toast.makeText(this, "테스트 토스트 테스트 토스트", Toast.LENGTH_SHORT).show() }
 
         mBinding.btnB.setOnClickListener { startActivity(Intent(this, SubActivity::class.java)) }
     }
