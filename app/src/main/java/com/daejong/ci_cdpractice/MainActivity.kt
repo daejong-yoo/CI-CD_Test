c2338711307fefdd44baa27f1c385160020151d9
daejong <teddy8653@gmail.com>
2026-02-28 16:22:17 +0900

    ci/cd test_mac 필요없는 소스 삭제

--- app/src/main/java/com/daejong/ci_cdpractice/MainActivity.kt
+++ app/src/main/java/com/daejong/ci_cdpractice/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.databinding.DataBindingUtil
 import com.daejong.ci_cdpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mBinding: ActivityMainBinding;
+    private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
